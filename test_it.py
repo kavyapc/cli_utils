08b7a7762457dfb259bc85ffcc1897e6e9e7fb35
kavyapc <36132470+kavyapc@users.noreply.github.com>
2026-03-11 16:44:31 -0600

    Update test_it.py

--- test_it.py
+++ test_it.py
@@ -1,4 +1,15 @@
 # test_it.py
-from cli_utils.separators import print_separator
+# from cli_utils.separators import print_separator
 
+# print_separator()
+
+# test_it.py
+from cli_utils import print_separator
+from cli_utils import cat
+from cli_utils import jollyroger
+from cli_utils import print_separator
+#from cli_utils import print_char_separator(char):
 print_separator()
+cat()
+jollyroger()
+#print_char_separator(*_*)
