47f65fe9fff58b22d8c409d79efe063366d04547
Kavya <kavyapc93@gmail.com>
2026-03-12 13:02:44 -0600

    Readme updated

--- cli_utils/separators.py
+++ cli_utils/separators.py
@@ -3,3 +3,12 @@
 def print_separator():
     """Prints a line of 30 asterisks to the terminal."""
     print("*" * 30)
+
+def print_custom_separator(char, length):
+    print(char * int(length))
+
+def print_box(message, char="*"):
+    length = len(message) + 4
+    print(char * length)
+    print(f"{char} {message} {char}")
+    print(char * length)
