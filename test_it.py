c12b2821953d9a3fc5e6f82919b6730b43485bdd
Kavya <kavyapc93@gmail.com>
2026-03-12 13:09:12 -0600

    updated testit

--- test_it.py
+++ test_it.py
@@ -1,4 +1,7 @@
 # test_it.py
-from cli_utils.separators import print_separator
 
-print_separator()
+import cli_utils.separators
+
+cli_utils.separators.print_separator()
+cli_utils.separators.print_custom_separator("kavya",4)
+cli_utils.separators.print_box("Welcome kavya")
